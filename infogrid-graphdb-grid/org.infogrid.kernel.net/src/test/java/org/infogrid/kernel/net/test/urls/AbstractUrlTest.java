ce5b542e58ebd7a720430927e9d6b1c5d35c1663
Johannes Ernst <jernst@indiecomputing.com>
2016-06-06 17:52:23 +0000

    Added missing test initialization

--- infogrid-graphdb-grid/org.infogrid.kernel.net/src/test/java/org/infogrid/kernel/net/test/urls/AbstractUrlTest.java
+++ infogrid-graphdb-grid/org.infogrid.kernel.net/src/test/java/org/infogrid/kernel/net/test/urls/AbstractUrlTest.java
@@ -5,7 +5,7 @@
 // have received with InfoGrid. If you have not received LICENSE.InfoGrid.txt
 // or you do not consent to all aspects of the license and the disclaimers,
 // no license is granted; do not use this file.
-// 
+//
 // For more information about InfoGrid go to http://infogrid.org/
 //
 // Copyright 1998-2015 by Johannes Ernst
@@ -14,11 +14,24 @@
 
 package org.infogrid.kernel.net.test.urls;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.diet4j.core.ModuleRegistry;
+import org.diet4j.core.ModuleRequirement;
+import org.diet4j.inclasspath.InClasspathModuleRegistry;
+import org.infogrid.mesh.net.a.DefaultAnetMeshObjectIdentifierFactory;
 import org.infogrid.modelbase.ModelBase;
 import org.infogrid.modelbase.ModelBaseSingleton;
 import org.infogrid.testharness.AbstractTest;
+import org.infogrid.util.ResourceHelper;
 import org.infogrid.util.context.Context;
 import org.infogrid.util.context.SimpleContext;
+import org.infogrid.util.logging.Dumper;
+import org.infogrid.util.logging.Log;
+import org.infogrid.util.logging.ToStringDumper;
+import org.infogrid.util.logging.log4j.Log4jLog;
+import org.infogrid.util.logging.log4j.Log4jLogFactory;
+import org.junit.BeforeClass;
 
 /**
  * Superclass for all tests in this package.
@@ -27,6 +40,25 @@ public abstract class AbstractUrlTest
         extends
             AbstractTest
 {
+    /**
+     * Initialize Module Framework, and initialize statics.
+     *
+     * @throws Exception all sorts of things may go wrong in tests
+     */
+    @BeforeClass
+    public static void initialize()
+        throws
+            Exception
+    {
+        ClassLoader    cl       = AbstractUrlTest.class.getClassLoader();
+        ModuleRegistry registry = InClasspathModuleRegistry.instantiateOrGet( cl );
+
+        registry.resolve( registry.determineSingleResolutionCandidate( ModuleRequirement.create( "org.infogrid", "org.infogrid.kernel" ))).activateRecursively();
+
+        Log4jLog.configure( "org/infogrid/kernel/net/test/urls/Log.properties", cl );
+        Log.setLogFactory( new Log4jLogFactory());
+    }
+
     /**
      * The ModelBase.
      */
